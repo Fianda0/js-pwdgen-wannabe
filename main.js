78aad922e1793107c48050699780375dc9754b5f
Fianda0 <fiandanesegiuseppe000@gmail.com>
2024-04-16 14:38:48 +0200

    aggiunta script

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-let inserireNome = prompt()
+let inserireNome = prompt('inserire il tuo nome')
 document.getElementById("nome").innerHTML = inserireNome
 
+let inserireCognome = prompt('Inserire il tuo cognome')
+document.getElementById("cognome").innerHTML = inserireCognome
+
+let inserireColore = prompt('Inserire il tuo colore preferito')
+document.getElementById("colore").innerHTML = inserireColore
+
+let password = inserireNome + inserireCognome + inserireColore + '#24'
+alert(password);
+document.getElementById('password').innerHTML = password
